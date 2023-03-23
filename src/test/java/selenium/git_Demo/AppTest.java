1324d03e84d1234e325122b0e406a4492ed00297
rutnag <rutujajay99@gmail.com>
2023-03-23 21:34:45 +0530

    first commit of Git_demo

---
+++ src/test/java/selenium/git_Demo/AppTest.java
@@ -0,0 +1,13 @@
+package selenium.git_Demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
